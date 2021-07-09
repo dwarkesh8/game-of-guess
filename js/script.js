61729c7b9b24e9e36e096bb0a709c4148eb2c47f
dwarkesh8 <dwarkeshpurohit8@gmail.com>
2021-07-09 14:34:05 +0530

    Set button action to navigate through screens, loader transition animation and giphy gif image

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
 $(document).ready(function(){
+	$("#loader").fadeOut(1000);
+	$("#main").fadeIn(1000);
+	$("#next").on("click", function(){
+		var playerName = $("input#playerName").val();
+		if (playerName.length > 0 && playerName != '' && playerName != null) {
+			$("#screen1").attr('hidden',true);
+			$("#screen2").attr('hidden',false);
+			$("#printPlayerName").text('Hi '+playerName);
+			$("#next").attr('hidden',true);
+			$("#start").attr('hidden',false);
+			setTimeout(function(){
+				var typingSound = document.getElementById('typingSound');
+				typingSound.play();
+			},200);
+		}
+		else {
+			$("input#playerName").focus();
+		}
+	});
+	$(".game-button").on("click", function(){
+		var clickSound = document.getElementById('clickSound');
+		clickSound.play();
+	});
+	$("#start").on("click", function(){
+		$("#loader").fadeIn(1000);
+		$("#main").fadeOut(1000);
+
+		setTimeout(function(){
+		$("#loader").fadeOut(1000);
+		$("#main").fadeIn(1000);
+
+		$("#screen1").attr('hidden',true);
+		$("#screen2").attr('hidden',true);
+		$("#screen3").attr('hidden',false);
+
+		$("#next").attr('hidden',true);
+		$("#start").attr('hidden',true);
+		$("#submitNumber").attr('hidden',false);
+		}, 3000);
+	});
 });
--- js/sound.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var typingSound = document.getElementById('typingSound');
-	typingSound.play();
-	$(".game-button").on("click", function(){
-		var clickSound = document.getElementById('clickSound');
-		clickSound.play();
-	});
-});
